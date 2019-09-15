e7f3603b5e31856423408307907a32b23594bd76
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-09-14 21:53:49 -0700

    Share states using context and useReducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {createContext, useReducer} from "react";
+
+import "./App.css";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Header from "./components/Header";
+export const AuthContext = createContext();
+
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+  token: null,
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      localStorage.setItem("user", JSON.stringify(action.payload.user));
+      localStorage.setItem("token", JSON.stringify(action.payload.token));
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user,
+        token: action.payload.token
+      };
+    case "LOGOUT":
+      localStorage.clear();
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+        token: null
+      };
+    default:
+      return state;
+  }
+};
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider value = {{state, dispatch}}>
+      <Header />
+  <div className="App">{!state.isAuthenticated ? <Login /> : <Home />}</div>
+    </AuthContext.Provider>
   );
 }
 
